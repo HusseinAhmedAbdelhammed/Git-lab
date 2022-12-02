d8f3b2ca6b6c2bdbe54649fd0d27e592b93deef4
Hussien <sonictwester@gmail.com>
2022-12-02 15:37:12 +0200

    fixing .gitignore

---
+++ HelloITI.java
@@ -0,0 +1,17 @@
+class HelloITI{
+	
+	public static void main(String args[]){
+	
+		System.out.println("hello world");
+		
+		printHelloGit();
+	}
+	
+	public static void printHelloGit(){
+		
+		System.out.println("hello git");
+		
+	}
+
+
+}
