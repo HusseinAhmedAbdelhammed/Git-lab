5889a6b0bf131ec00fbd5dd8004450091acbb3c6
Hussien <sonictwester@gmail.com>
2022-12-02 14:57:31 +0200

    added mod java file

--- HelloITI.java
+++ HelloITI.java
@@ -3,7 +3,14 @@ class HelloITI{
 	public static void main(String args[]){
 	
 		System.out.println("hello world");
+		
+		printHelloGit();
+	}
 	
+	public static void printHelloGit(){
+		
+		System.out.println("hello git");
+		
 	}
 
 
